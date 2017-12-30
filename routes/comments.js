9de8512595261c3e3d14351e6080e9c55f6d3493
208-209 <208_209@outlook.jp>
2017-12-30 10:58:23 +0000

    コメントの削除機能の実装

--- routes/comments.js
+++ routes/comments.js
@@ -7,14 +7,23 @@ const Comment = require('../models/comment');
 
 router.post('/:gameId/comments', authenticationEnsurer, (req, res, next) => {
   const gameId = req.params.gameId;
-  Comment.create({
-    comment: req.body.comment,
-    trackingCookie: '0123',
-    gameId: gameId,
-    postedBy: req.user.id
-  }).then(() => {
-    res.redirect('/games/' + gameId);
-  });
+  if (parseInt(req.query.delete) === 1) {
+    Comment.findById(req.body.id).then((comment) => {
+      if (req.user.id === comment.postedBy) {
+        comment.destroy();
+      }
+      res.redirect('/games/' + gameId);
+    });
+  } else {
+    Comment.create({
+      comment: req.body.comment,
+      trackingCookie: '0123',
+      gameId: gameId,
+      postedBy: req.user.id
+    }).then(() => {
+      res.redirect('/games/' + gameId);
+    });
+  }
 });
 
 module.exports = router;
