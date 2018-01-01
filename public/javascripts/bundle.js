05ee5c190b2238db0e9831e483f6d83b0dea0b92
208-209 <208_209@outlook.jp>
2018-01-01 09:34:31 +0000

    isLetter関数のターゲットの指定をjQueryに変更

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -10511,9 +10511,10 @@ $('.playGame').each((i, e) => {
 
     function isLetter() {
       const currentKeyCode = currentContent[currentNumber] ? currentContent[currentNumber].charCodeAt() : '';
-      $('.key').removeClass('letter');
-      $('.key_' + currentKeyCode).addClass('letter');
-      modalContent.html(`<span>${currentContent.substring(0, currentNumber)}</span><span style="color: red">${currentContent[currentNumber]}</span><span>${currentContent.substring(currentNumber + 1)}</span>`);
+      $('.key').removeClass('isKey');
+      $('.key_' + currentKeyCode).addClass('isKey');
+      modalContent.html(`<span>${currentContent.substring(0, currentNumber)}</span><span id="isLetter">${currentContent[currentNumber]}</span><span>${currentContent.substring(currentNumber + 1)}</span>`);
+      $('#isLetter').css('color', 'orange');
       console.log(currentContent[currentNumber]);
       console.log(currentKeyCode);
     }
