e16f28d96fc9dc9a10ab423dc7d382790a237ece
208-209 <208_209@outlook.jp>
2018-01-09 19:47:12 +0000

    Userにimageを保存しないよう変更

--- models/user.js
+++ models/user.js
@@ -11,10 +11,6 @@ const User = loader.database.define('users', {
   username: {
     type: Sequelize.STRING,
     allowNull: false
-  },
-  image: {
-    type: Sequelize.STRING,
-    allowNull: false
   }
 }, {
     freezeTableName: true,
