65398ec789352217700532fe556dcd4f4f22df43
208-209 <208_209@outlook.jp>
2018-01-14 10:57:38 +0000

    navbar のユーザーの表示を when case での方法に変更

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
 'use strict';
 const request = require('supertest');
 const app = require('../app');
+const passportStub = require('passport-stub');
 
 describe('/login', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ username: 'testuser' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
 
   it('ログインのためのリンクが含まれる', (done) => {
     request(app)
@@ -14,4 +24,19 @@ describe('/login', () => {
       .expect(200, done);
   });
 
+  it('ログイン時はユーザー名が表示される', (done) => {
+    request(app)
+      .get('/login')
+      .expect(/testuser/)
+      .expect(200, done);
+  });
+});
+
+describe('/logout', () => {
+  it('/ にリダイレクトされる', (done) => {
+    request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302, done);
+  });
 });
