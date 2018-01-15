f2c00b97bb25335566be7e5490cab67c7a14c57a
208-209 <208_209@outlook.jp>
2018-01-15 08:51:23 +0000

    gameのindexesにupdatedAtを追加

--- models/game.js
+++ models/game.js
@@ -29,7 +29,7 @@ const Game = loader.database.define('games', {
     timestamps: true,
     indexes: [
       {
-        fields: ['gameName', 'tags', 'createdBy', 'privacy']
+        fields: ['gameName', 'tags', 'createdBy', 'privacy', 'updatedAt']
       }
     ]
   });
