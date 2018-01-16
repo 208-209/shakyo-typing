73e951132f11521a7b09b9aa68e3ec1ca068e0fb
208-209 <208_209@outlook.jp>
2018-01-16 18:33:10 +0000

    コメントの削除にクエリを使わないよう変更

--- routes/comments.js
+++ routes/comments.js
@@ -7,40 +7,39 @@ const Comment = require('../models/comment');
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
 
-// コメントの投稿・削除
+// コメントの投稿
 router.post('/:gameId/comments', authenticationEnsurer, csrfProtection, (req, res, next) => {
-  const gameId = req.params.gameId;
-  if (parseInt(req.query.delete) === 1) {
-    Comment.findById(req.body.commentId).then((comment) => {
-      // 投稿者 または 管理人
-      if (util.isMine(req, comment) || util.isAdmin(req)) {
-        comment.destroy();
-        console.info(
-          `【コメントの削除】user: ${req.user.username}, ${req.user.provider}, ${req.user.id} ` +
-          `remoteAddress: ${req.connection.remoteAddress}, ` +
-          `userAgent: ${req.headers['user-agent']} `
-        );
-        res.redirect('/games/' + gameId);
-      } else {
-        const err = new Error('削除する権限がありません');
-        err.status = 404;
-        next(err);
-      }
-    });
-  } else {
-    Comment.create({
-      comment: req.body.comment,
-      gameId: gameId,
-      createdBy: req.user.id
-    }).then(() => {
-      res.redirect('/games/' + gameId);
+  Comment.create({
+    comment: req.body.comment,
+    gameId: req.params.gameId,
+    createdBy: req.user.id
+  }).then(() => {
+    res.redirect('/games/' + req.params.gameId);
+    console.info(
+      `【コメントの投稿】user: ${req.user.username}, ${req.user.provider}, ${req.user.id} ` +
+      `remoteAddress: ${req.connection.remoteAddress}, ` +
+      `userAgent: ${req.headers['user-agent']} `
+    );
+  });
+});
+
+// コメントの削除
+router.post('/:gameId/comments/delete', authenticationEnsurer, csrfProtection, (req, res, next) => {
+  Comment.findById(req.body.commentId).then((comment) => {
+    if (util.isMine(req, comment) || util.isAdmin(req)) { // 投稿者 または 管理人
+      comment.destroy();
       console.info(
-        `【コメントの投稿】user: ${req.user.username}, ${req.user.provider}, ${req.user.id} ` +
+        `【コメントの削除】user: ${req.user.username}, ${req.user.provider}, ${req.user.id} ` +
         `remoteAddress: ${req.connection.remoteAddress}, ` +
         `userAgent: ${req.headers['user-agent']} `
       );
-    });
-  }
+      res.redirect('/games/' + req.params.gameId);
+    } else {
+      const err = new Error('削除する権限がありません');
+      err.status = 404;
+      next(err);
+    }
+  });
 });
 
 module.exports = router;
