ee20b10a23d2e6ac7de85f6272d83b64b5a14c5f
208-209 <208_209@outlook.jp>
2018-01-24 13:32:15 +0000

    ログイン時はユーザー名が表示される テストの実装

--- test/test.js
+++ test/test.js
@@ -4,15 +4,6 @@ const app = require('../app');
 const passportStub = require('passport-stub');
 
 describe('/login', () => {
-  before(() => {
-    passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
-  });
-
-  after(() => {
-    passportStub.logout();
-    passportStub.uninstall(app);
-  });
 
   it('ログインのためのリンクが含まれる', (done) => {
     request(app)
@@ -23,14 +14,25 @@ describe('/login', () => {
       .expect(/<a href="\/auth\/github"/)
       .expect(200, done);
   });
-  /*
+});
+
+describe('/login', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ username: 'testuser' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
   it('ログイン時はユーザー名が表示される', (done) => {
     request(app)
       .get('/login')
       .expect(/testuser/)
       .expect(200, done);
   });
-  */
 });
 
 describe('/logout', () => {
