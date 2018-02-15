bf8bf10f5bdf1f907beefde024b766bba914aada
208-209 <208_209@outlook.jp>
2018-02-16 00:42:43 +0900

    comment.jsのインデックスに createdBy を追加

--- models/comment.js
+++ models/comment.js
@@ -26,7 +26,7 @@ const Comment = loader.database.define('comments', {
     timestamps: true,
     indexes: [
       {
-        fields: ['gameId'] // createdBy(FK)
+        fields: ['createdBy', 'gameId']
       }
     ]
   });
