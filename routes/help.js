4d217e6caf33ad3de27d8d6c1d3c610ed6133607
208-209 <208_209@outlook.jp>
2018-02-20 23:28:47 +0900

    モジュールの追加

--- routes/help.js
+++ routes/help.js
@@ -1,6 +1,14 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const util = require('./util.js');
+const loader = require('../models/sequelize-loader');
+const sequelize = loader.database;
+const Game = require('../models/game');
+const Stage = require('../models/stage');
+const Favorite = require('../models/favorite');
+const Like = require('../models/like');
+const Comment = require('../models/comment');
 
 router.get('/', (req, res, next) => {
   res.render('help', { user: req.user });
