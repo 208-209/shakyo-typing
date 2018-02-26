39f511a02c36145041340434443667897a333075
208-209 <208_209@outlook.jp>
2018-02-27 01:42:38 +0900

    ゲーム作成のテストを実装（failing）

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+const User = require('../models/user');
 
 describe('/login', () => {
   it('ログインのためのリンクが含まれる', (done) => {
@@ -42,3 +43,44 @@ describe('/logout', () => {
       .expect(302, done);
   });
 });
+
+/*
+describe('/games', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ id: '0', username: 'testuser', image: 'testImage' });
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('ゲームが作成でき、表示される', (done) => {
+    User.upsert({ userId: '0', username: 'testuser', image: 'testImage' }).then(() => {
+      request(app)
+        .post('/games')
+        .send({ gameName: 'テストゲーム', tags: 'タグ1\r\nタグ2', privacy: 0 })
+        .expect('Location', /games/)
+        .expect(302)
+        .end((err, res) => {
+          let createdGamePath = res.headers.location;
+          request(app)
+            .get(createdGamePath)
+            .expect(/テストゲーム/)
+            .expect(/タグ1/)
+            .expect(/タグ2/)
+            .expect(/非公開/)
+            .expect(200)
+            .end((err, res) => {
+              // テストで作成したデータを削除
+              let gameId = createdSchedulePath.split('/games/')[1];
+              Game.findById(gameId).then((g) => { g.destroy(); });
+              if (err) return done(err);
+              done();
+            });
+        });
+    });
+  });
+});
+*/
